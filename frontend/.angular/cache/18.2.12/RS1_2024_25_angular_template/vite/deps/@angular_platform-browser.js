0b98f157438a5c1eeb637e139848b60dc9964856
Marko Dogan <marko.dogan@edu.fit.ba>
2025-01-25 02:17:42 +0100

    home page fix

--- frontend/.angular/cache/18.2.12/RS1_2024_25_angular_template/vite/deps/@angular_platform-browser.js
+++ frontend/.angular/cache/18.2.12/RS1_2024_25_angular_template/vite/deps/@angular_platform-browser.js
@@ -41,8 +41,8 @@ import {
   getDOM
 } from "./chunk-QFPSIRYE.js";
 import "./chunk-LO7GNJQE.js";
-import "./chunk-4N4GOYJH.js";
 import "./chunk-5OPE3T2R.js";
+import "./chunk-4N4GOYJH.js";
 import "./chunk-FHTVLBLO.js";
 import "./chunk-KBUIKKCC.js";
 export {
